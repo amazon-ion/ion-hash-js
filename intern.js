cfc8aeabe5e5d24d8afe107c752b75fd19411e79
Peter Cornell <pcornell@amazon.com>
2019-06-28 14:46:42 -0700

    test_byte_array_comparator cleanup

--- intern.js
+++ intern.js
@@ -1,10 +1,6 @@
 define({
-  defaultTimeout: 2000, // ms
-  excludeInstrumentation: /^(?:tests|node_modules)\//,
-  filterErrorStack: true,
-  //suites: [
-    //'tests/unit/test_byte_array_comparator',
-    //'tests/unit/test_escape',
-  //],
+  "defaultTimeout": 2000,
+  "excludeInstrumentation": /^(?:tests|node_modules)\//,
+  "filterErrorStack": true,
 });
 
--- tests/test_byte_array_comparator.ts
+++ tests/test_byte_array_comparator.ts
@@ -5,40 +5,26 @@ define([
         'dist/amd/es6/IonHash',
     ],
     function(intern, registerSuite, assert, ionhash) {
-        var suite = {
-            name: 'byteArrayComparator'
-        };
+        registerSuite({
+            name: "byteArrayComparator",
+            equals: () => { test([0x01, 0x02, 0x03], [0x01, 0x02, 0x03], 0); },
+            lessThan: () => { test([0x01, 0x02, 0x03], [0x01, 0x02, 0x04], -1); },
+            lessThanDueToLength: () => { test([0x01, 0x02, 0x03], [0x01, 0x02, 0x03, 0x04], -1); },
+            greaterThan: () => { test([0x01, 0x02, 0x04], [0x01, 0x02, 0x03], 1); },
+            greaterThanDueToLength: () => { test([0x01, 0x02, 0x03, 0x04], [0x01, 0x02, 0x03], 1); },
+            unsignedBehavior: () => {
+                test([0x01], [0x7f], -1);
+                test([0x01], [0x80], -1);
+                test([0x01], [0xff], -1);
+                test([0x7f], [0x01], 1);
+                test([0x80], [0x01], 1);
+                test([0xff], [0x01], 1);
+            },
+        });
 
-        suite['equals'] = function() {
-            assert.equal(ionhash.byteArrayComparator([0x01, 0x02, 0x03], [0x01, 0x02, 0x03]), 0);
+        let test = function(a, b, expected) {
+            assert.equal(ionhash.byteArrayComparator(a, b), expected);
         };
-
-        suite['lessThan'] = function() {
-            assert.equal(ionhash.byteArrayComparator([0x01, 0x02, 0x03], [0x01, 0x02, 0x04]), -1);
-        };
-
-        suite['lessThanDueToLength'] = function() {
-            assert.equal(ionhash.byteArrayComparator([0x01, 0x02, 0x03], [0x01, 0x02, 0x03, 0x04]), -1);
-        };
-
-        suite['greaterThan'] = function() {
-            assert.equal(ionhash.byteArrayComparator([0x01, 0x02, 0x04], [0x01, 0x02, 0x03]), 1);
-        };
-
-        suite['greaterThanDueToLength'] = function() {
-            assert.equal(ionhash.byteArrayComparator([0x01, 0x02, 0x03, 0x04], [0x01, 0x02, 0x03]), 1);
-        };
-
-        suite['unsignedBehavior'] = function() {
-            assert.equal(ionhash.byteArrayComparator([0x01], [0x7f]), -1);
-            assert.equal(ionhash.byteArrayComparator([0x01], [0x80]), -1);
-            assert.equal(ionhash.byteArrayComparator([0x01], [0xff]), -1);
-            assert.equal(ionhash.byteArrayComparator([0x7f], [0x01]), 1);
-            assert.equal(ionhash.byteArrayComparator([0x80], [0x01]), 1);
-            assert.equal(ionhash.byteArrayComparator([0xff], [0x01]), 1);
-        };
-
-        registerSuite(suite);
     }
 );
 
