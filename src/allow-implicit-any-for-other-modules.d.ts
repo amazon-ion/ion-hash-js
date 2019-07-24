1b1cf143fb6dcbb79673d9f539583f1049c034e6
Peter Cornell <pcornell@amazon.com>
2019-07-23 18:09:44 -0700

    clarified purpose of src/allow-implicit-any-for-other-modules.d.ts

--- src/allow-implicit-any-for-other-modules.d.ts
+++ src/allow-implicit-any-for-other-modules.d.ts
@@ -1,2 +1,8 @@
+// this file exists to work-around the following tsc error:
+//
+//    ../ion-js.development/dist/commonjs/es6/IonLongInt.d.ts(1,28): error TS7016: Could not find a declaration file for module './BigInteger'. '/Volumes/Unix/dev/ion/ion-js.development/dist/commonjs/es6/BigInteger.js' implicitly has an 'any' type.
+//
+// once that's addressed, consider removing this file
+//
 declare module '*';
 
