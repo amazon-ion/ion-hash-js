042c258457e9419c1cd7254c6b74a8fdcea0b4f9
Peter Cornell <pcornell@amazon.com>
2019-07-24 14:29:38 -0700

    adds issue reference for allow-implicit-any-for-other-modules.d.ts

--- src/allow-implicit-any-for-other-modules.d.ts
+++ src/allow-implicit-any-for-other-modules.d.ts
@@ -1,3 +1,5 @@
+// https://github.com/amzn/ion-hash-js/issues/7
+//
 // this file exists to work-around the following tsc error:
 //
 //    ../ion-js.development/dist/commonjs/es6/IonLongInt.d.ts(1,28): error TS7016: Could not find a declaration file for module './BigInteger'. '/Volumes/Unix/dev/ion/ion-js.development/dist/commonjs/es6/BigInteger.js' implicitly has an 'any' type.
